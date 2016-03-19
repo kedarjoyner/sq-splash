48bf364f92c81fc2ffff6f6f746d39baf239d3ea
kedarjoyner <kedarjoyner@gmail.com>
2016-03-19 11:22:11 -0500

    change http_path

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "http://www.kedarcodes.com"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
@@ -17,7 +17,7 @@ output_style = :compressed
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
